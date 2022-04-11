fa6937202027349a08ac50da2ed613e4ac502b2b
PB12203006 <hm2689@columbia.edu>
2022-04-10 18:07:05 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import web3 from "./web3";
+import lottery from "./lottery";
 
 function App() {
+  const [manager, setManager] = useState("");
+  const [players, setPlayers] = useState([]);
+  const [balance, setBalance] = useState("0");
+  const [toBeDeposited, setToBeDeposited] = useState("0");
+  const [account, setAccount] = useState("");
+  const getData = async () => {
+    const manager = await lottery.methods.manager().call();
+    setManager(manager);
+
+    const players = await lottery.methods.getPlayers().call();
+    setPlayers(players);
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    setBalance(balance);
+    const account = (await web3.eth.getAccounts())[0];
+    setAccount(account);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const deposit = async () => {
+    await lottery.methods.enter().send({
+      from: account,
+      value: web3.utils.toWei(toBeDeposited, "ether"),
+    });
+    alert("deposited");
+    getData();
+  };
+
+  const pickWinner = async () => {
+    await lottery.methods.pickWinner().send({
+      from: account,
+    });
+    getData();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +48,19 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>constract manager: {manager}</p>
+        <p>playsers # {players.length}</p>
+        <p>compete to win {web3.utils.fromWei(balance, "ether")}</p>
+        <h5>deposit to win</h5>
+        <input
+          placeholder="amount in ether(minimum 0.01)"
+          value={toBeDeposited}
+          onChange={(event) => setToBeDeposited(event.target.value)}
+        />
+        <button onClick={deposit}>deposit</button>
+        {account === manager && (
+          <button onClick={pickWinner}>pickWinner</button>
+        )}
       </header>
     </div>
   );
---
+++ src/lottery.js
@@ -0,0 +1,72 @@
+import web3 from "./web3";
+
+const address = "0xE2FE9042894faE8553F46A0c5C6C4D84F97b51BB";
+
+const abi = [
+  {
+    inputs: [],
+    name: "enter",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "pickWinner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "getPlayers",
+    outputs: [
+      {
+        internalType: "address payable[]",
+        name: "",
+        type: "address[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "manager",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    name: "players",
+    outputs: [
+      {
+        internalType: "address payable",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
+
+export default new web3.eth.Contract(abi, address);
